83567dfda177496f41dd09101bce2cd82c58a07c
Mindaugas Vaiciunas <vaiciunas.min@gmail.com>
2015-10-11 16:49:09 +0300

    Clean up code

--- tableViewExample/ViewController.swift
+++ tableViewExample/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  tableViewExample
-//
-//  Created by Mindaugas Vaiciunas on 11/10/15.
-//  Copyright © 2015 Mindaugas Vaiciunas. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,12 +6,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
