26aa04404119c45523c059869630fd94b6584ee1
Mindaugas Vaiciunas <vaiciunas.min@gmail.com>
2015-10-12 01:02:16 +0300

    Implement delete and add functionality to table view

--- tableViewExample/ViewController.swift
+++ tableViewExample/ViewController.swift
@@ -35,6 +35,35 @@ class ViewController: UIViewController, UITableViewDataSource {
         }
         return cell
     }
+    
+    @IBAction func addNew()  {
+        let indexPath = NSIndexPath(forRow: allNames.count, inSection: 1)
+        allNames = allNames.arrayByAddingObject(NSDate().description)
+        tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+    }
+    
+    @IBAction func edit()  {
+        tableView.setEditing(!tableView.editing, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            switch (indexPath.section) {
+            case 0:
+                let mutableArray :NSMutableArray = allTitles.mutableCopy() as! NSMutableArray
+                mutableArray.removeObjectAtIndex(indexPath.row)
+                allTitles = mutableArray.copy() as! NSArray
+            case 1:
+                let mutableArray :NSMutableArray = allNames.mutableCopy() as! NSMutableArray
+                mutableArray.removeObjectAtIndex(indexPath.row)
+                allNames = mutableArray.copy() as! NSArray
+            default:
+                break
+            }
+            
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
 }
 
 
